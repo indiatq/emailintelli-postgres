b902d81b77c6ada33916d94ba96d0466b8d78b16
unknown <godlyabraham@gmail.com>
2024-10-11 11:32:27 +0530

    initial commit

--- src/lib/embeddings.ts
+++ src/lib/embeddings.ts
@@ -8,6 +8,9 @@ const openai = new OpenAIApi(config);
 
 export async function getEmbeddings(text: string) {
     try {
+        console.log("Value of text ",text);
+        console.log("OPENAI_API_KEY =",process.env.OPENAI_API_KEY);
+
         const response = await openai.createEmbedding({
             model: "text-embedding-ada-002",
             input: text.replace(/\n/g, " "),
